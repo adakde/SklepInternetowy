d8e3b4cd9202dfbbe43841dd0e8747b1b48ecd58
PC <PC@DESKTOP-ES2QSNP>
2024-11-18 19:39:35 +0100

    Dodaj pliki projektów.

---
+++ SklepInternetowy/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SklepInternetowy.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [MaxLength(50)]
+        public string Brand { get; set; } = "";
+        [MaxLength(50)]
+        public string Category { get; set; } = "";
+        [Precision(16,2)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = "";
+        [MaxLength(50)]
+        public string ImageFileName { get; set; } = "";
+        
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ SklepInternetowy/Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SklepInternetowy.Models;
+
+namespace SklepInternetowy.Services
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
